77197dda64515bf36bb151c698400e417b83af3e
Tijs van Lieshout <tijsvanlieshout@gmail.com>
2019-09-23 20:48:20 +0200

    Basic PySimpleGui

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello world!")
+import PySimpleGUI as sg
 
+layout = [[sg.Text('Your typed chars appear here:'), sg.Text('', key='_OUTPUT_')],
+          [sg.Input(key='_IN_')],
+          [sg.Button('Show'), sg.Button('Exit')]]
 
-if __name__ == '__main__':
-    main()
+window = sg.Window('Window Title', layout)
+
+while True:  # Event Loop
+    event, values = window.Read()
+    print(event, values)
+    if event in (None, 'Exit'):
+        break
+    if event == 'Show':
+        # change the "output" element to be the value of "input" element
+        window.Element('_OUTPUT_').Update(values['_IN_'])
+
+window.Close()
