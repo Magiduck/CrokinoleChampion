5eb9189504664aeca5ff205ca8b1ec94de5cff38
Tijs van Lieshout <tijsvanlieshout@gmail.com>
2019-10-04 11:19:56 +0200

    prevented crash behaviour pressing buttons out of order; added beginning implementation play offs window

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ column_schedule = [[sg.Radio('Half-season', 'season', key='_HALFSEASON_')],
                    [sg.Button('Play season!', key='_PLAYSEASON_')]]
 
 layout = [[sg.Text('Input players\t\tSchedule              ', key='_MENUTEXT_')],
-          [sg.Multiline(size=(22, 20), key='_CONTESTANTSIN_'),
-           sg.Multiline(size=(22, 20), disabled=True, key='_CONTESTANTSOUT_'), sg.Column(column_schedule)],
+          [sg.Multiline(size=(22, 10), font='Consolas 18', key='_CONTESTANTSIN_'),
+           sg.Multiline(size=(22, 10), font='Consolas 18', disabled=True, key='_CONTESTANTSOUT_'), sg.Column(column_schedule)],
           [sg.Text('Red - Black\n 0 - 0', font='Consolas 48', size=(16, 3), key='_SCORE_')],
           [sg.Button('Red won', key='_RED_'), sg.Button('Draw', key='_DRAW_'), sg.Button('Black won', key='_BLACK_')],
           [sg.Button('Finish', key='_FINISH_'), sg.Button('Undo', key='_UNDO_')],
@@ -25,15 +25,17 @@ matches = []
 prev_score_text = ''
 score_text = ''
 rankings_text = ''
-
+win2_active = False
+matches_created = False
+season_active = False
 while True:  # Event Loop
     event, values = window.Read()
-    print(event, values)
     if event in (None, 'Exit'):
         break
 
     # If the user has clicked on 'create matches'
     if event == '_CREATEMATCHES_':
+        matches_created = True
         # Get the matches
         output_text, matches = Matches.create_match_schedule(values['_CONTESTANTSIN_'], values['_HALFSEASON_'], values['_FULLSEASON_'])
         # Update the multiline text output with the matches
@@ -41,13 +43,14 @@ while True:  # Event Loop
         # Update the menu with the amount of matches
         window['_MENUTEXT_'].Update(f'Input players\t\tSchedule ({len(matches)} matches)')
 
-    elif event == '_SHUFFLE_':
+    elif event == '_SHUFFLE_' and matches_created:
         # Shuffle the matches
         output_text, matches = Matches.shuffle_matches(matches)
         # Update the multiline text output with the matches
         window['_CONTESTANTSOUT_'].Update(output_text)
 
-    elif event == '_PLAYSEASON_':
+    elif event == '_PLAYSEASON_' and matches_created:
+        season_active = True
         # Get first match
         score_text, match, matches = Matches.initialise_match(matches)
         # Update the score text
@@ -59,7 +62,7 @@ while True:  # Event Loop
         # Update the multiline text output with the rankings
         window['_RANKINGS_'].Update(initial_rankings)
 
-    elif event == '_RED_':
+    elif event == '_RED_' and season_active:
         # To be able to undo
         prev_score_text = score_text
 
@@ -67,7 +70,7 @@ while True:  # Event Loop
         score_text = Rankings.update_score(score_text, 'RED')
         window['_SCORE_'].Update(score_text)
 
-    elif event == '_DRAW_':
+    elif event == '_DRAW_' and season_active:
         # To be able to undo
         prev_score_text = score_text
 
@@ -75,7 +78,7 @@ while True:  # Event Loop
         score_text = Rankings.update_score(score_text, 'DRAW')
         window['_SCORE_'].Update(score_text)
 
-    elif event == '_BLACK_':
+    elif event == '_BLACK_' and season_active:
         # To be able to undo
         prev_score_text = score_text
 
@@ -83,7 +86,7 @@ while True:  # Event Loop
         score_text = Rankings.update_score(score_text, 'BLACK')
         window['_SCORE_'].Update(score_text)
 
-    elif event == '_FINISH_':
+    elif event == '_FINISH_' and season_active:
         rankings_text = Rankings.update_rankings(rankings_text, score_text)
         # Update the multiline text output with the rankings
         window['_RANKINGS_'].Update(rankings_text)
@@ -94,13 +97,27 @@ while True:  # Event Loop
             output_text, matches = Matches.remove_last_match(matches)
             # Update the multiline text output with the matches
             window['_CONTESTANTSOUT_'].Update(output_text)
+        elif len(matches) == 1 and not win2_active:
+            win2_active = True
+            window.Hide()
+            layout2 = [[sg.Text('Rankings:')],
+                       [sg.Multiline(rankings_text, size=(22, 10), disabled=True, font='Consolas 18', key='_RANKINGS2_')]]
+
+            win2 = sg.Window('Playoffs', layout2)
+            while True:
+                event2, values2 = win2.Read()
+                if event2 in (None, 'Exit'):
+                    win2.Close()
+                    win2_active = False
+                    window.UnHide()
+                    break
 
         # Get next match
         score_text, match, matches = Matches.initialise_match(matches)
         # Update the score text
         window['_SCORE_'].Update(score_text)
 
-    elif event == '_UNDO_':
+    elif event == '_UNDO_' and season_active:
         score_text = prev_score_text
         window['_SCORE_'].Update(score_text)
 
